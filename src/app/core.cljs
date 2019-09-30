08c497c85a73814a3de382dc65fddb5a8b128609
pedroteixeira <pedroteixeira@petrobras.com.br>
2019-09-30 18:35:14 -0300

    Primeiras alterações

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,9 +1,30 @@
 (ns app.core
   (:require [reagent.core :as r]))
 
+(def data {:prefixes ["Air" "Jet" "Flight"]
+           :sufixes ["Hub" "Station" "Mart"]
+           :domains ["Airhub" "AirStation" "AirMart" "JetHub" 
+                     "JetStation" "JetMart" "FlightHub" "FlighStation" 
+                     "FlightMart"]})
+
+
+(def info (r/atom "Pedro "))
+
+(defn greeting [message]
+  [:h1 (str message " playboy!")])
+
 (defn app
   []
-  [:div.container])
+  [:div.container 
+   [:div.row 
+    [:div.col-md 
+     [:h5 @info 
+      [:span {:class "badge badge-info"} (count data)]]
+     [:div.card 
+      [:div.card-body 
+       [:ul.list-group 
+        (for [prefix (:prefixes data)]
+          [:li.list-group-item prefix])]]]]]])
 
 (defn ^:dev/after-load start
   []
@@ -13,3 +34,4 @@
 (defn ^:export init
   []
   (start))
+
